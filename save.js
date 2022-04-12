0d331c516e95255b35f65fc07fc87ffe9daf8c66
Saerom Park <park.srome@gmail.com>
2022-04-12 13:51:49 -0500

    init

---
+++ save.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', () => {
+  const textarea = document.querySelector('textarea');
+  textarea.focus();
+  const savedText = localStorage.getItem('text');
+  if (savedText !== null) {
+    textarea.value = savedText;
+  }
+  const savedWidth = localStorage.getItem('width');
+  if (savedWidth !== null) {
+    textarea.style.width = `${savedWidth}px`;
+  }
+  const savedHeight = localStorage.getItem('height');
+  if (savedHeight !== null) {
+    textarea.style.height = `${savedHeight}px`;
+  }
+  textarea.addEventListener('input', () => {
+    localStorage.setItem('text', textarea.value);
+  });
+  const saveSize = () => {
+    localStorage.setItem('width', textarea.offsetWidth);
+    localStorage.setItem('height', textarea.offsetHeight);
+  };
+  new ResizeObserver(saveSize).observe(textarea);
+});
